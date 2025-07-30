55e3d293f2460f501e75fee2b735ead3071ca1c0
Gustavo <gl.feio@campus.fct.unl.pt>
2025-07-30 12:42:02 +0100

    Added pause and gameover overlays.
    Revamped rendering... not sure how to feel about it.

--- src/main.c
+++ src/main.c
@@ -35,18 +35,24 @@
 #define WINDOW_WIDTH (UNIT*GRID_WIDTH)
 #define WINDOW_HEIGHT (UNIT*GRID_HEIGHT)
 
+#define BACKGROUND_COLOR 0x181818FF
+#define APPLE_COLOR 0xFF1818FF
+
 void *cnake_realloc(void *items, size_t bytes)
 {
 #ifdef CNAKE_DEBUG
-	fprintf(stderr, "Reallocated %zu bytes\n", bytes);
+	printf("DEBUG: Reallocated %zu bytes\n", bytes);
 #endif
 	return realloc(items, bytes);
 }
 
+#define DA_INIT_CAPACITY 16
+#define DA_GROWTH_FACTOR 1.5
+
 #define da_append(xs, x) \
 	do { \
 		if ((xs)->count >= (xs)->capacity) { \
-			(xs)->capacity = (xs)->capacity == 0 ? 16 : (xs)->capacity * 2; \
+			(xs)->capacity = (xs)->capacity == 0 ? DA_INIT_CAPACITY : (xs)->capacity * DA_GROWTH_FACTOR; \
 			(xs)->items = cnake_realloc((xs)->items, (xs)->capacity * sizeof((xs)->items[0])); \
 		} \
 		(xs)->items[(xs)->count++] = (x); \
@@ -80,142 +86,19 @@ typedef struct {
 	Apple apple;
 } Scene;
 
+typedef enum {
+	MENU,
+	PLAYING,
+	PAUSED,
+	GAMEOVER,
+} GameState;
+
 typedef struct {
 	Scene scene;
 	size_t score;
-	bool running;
-	bool gameover;
+	GameState state;
 	bool quit;
 } Game;
-
-static const char *const WINDOW_TITLE = "Cnake Game";
-
-void init_snake(Snake *snake)
-{
-	snake->dx = 1;
-	snake->dy = 0;
-	snake->dir_changed = false;
-	snake->count = 0;
-
-	// We update the game once before rendering, so we start at -1
-	for (size_t i = 0; i < SNAKE_INIT_LENGTH; i++) {
-		da_append(snake, ((SnakeBody) { i-1, 0, i+1 }));
-	}
-	snake->tail = 0;
-	snake->head = snake->count - 1;
-}
-
-Snake new_snake()
-{
-	Snake snake = {0};
-	init_snake(&snake);
-	return snake;
-}
-
-bool intersects_snake(Snake *snake, Vec2 rect)
-{
-	for (size_t i = 0; i < snake->count; i++) {
-		SnakeBody cur = snake->items[i];
-		if (cur.x == rect.x && cur.y == rect.y) return true;
-	}
-	return false;
-}
-
-Apple new_apple(Scene *scene)
-{
-	Apple apple;
-	do {
-		apple.x = (rand() / (double) RAND_MAX) * (GRID_WIDTH - 1);
-		apple.y = (rand() / (double) RAND_MAX) * (GRID_HEIGHT - 1);
-	} while (intersects_snake(&scene->snake, apple));
-	return apple;
-}
-
-void init_scene(Game *game)
-{
-	game->scene.snake = new_snake();
-	game->scene.apple = new_apple(&game->scene);
-}
-
-bool init_game(Game *game)
-{
-	srand(time(NULL));
-
-	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
-	SetTargetFPS(FPS);
-	SetExitKey(KEY_NULL);
-
-	init_scene(game);
-
-	game->running = true;
-	game->gameover = false;
-	game->quit = false;
-
-	return true;
-}
-
-void update_scene(Game *game)
-{
-	Scene *scene = &game->scene;
-	Snake *snake = &scene->snake;
-	size_t tail = snake->tail;
-	size_t head = snake->head;
-	SnakeBody old_tail = snake->items[tail];
-	SnakeBody old_head = snake->items[head];
-
-	snake->tail = snake->items[tail].next;
-	// tail->next = NULL;
-	snake->items[head].next = tail;
-	snake->head = tail;
-	head = tail;
-
-	snake->items[head].x = old_head.x + snake->dx;
-	snake->items[head].y = old_head.y + snake->dy;
-
-	if (snake->items[head].x == scene->apple.x && snake->items[head].y == scene->apple.y) {
-		da_append(snake, ((SnakeBody) { old_tail.x, old_tail.y, snake->tail }));
-		snake->tail = snake->count - 1;
-		scene->apple = new_apple(scene);
-		game->score += 1;
-	}
-	snake->dir_changed = false;
-}
-
-bool out_of_bounds(Snake *snake)
-{
-	SnakeBody head = snake->items[snake->head];
-	return head.x >= GRID_WIDTH || head.x < 0 || head.y >= GRID_HEIGHT || head.y < 0;
-}
-
-bool ouroboros(Snake *snake)
-{
-	SnakeBody head = snake->items[snake->head];
-	size_t cur = snake->tail;
-	while (cur != snake->head) {
-		SnakeBody b = snake->items[cur];
-		if (b.x == head.x && b.y == head.y) return true;
-		cur = b.next;
-	}
-	return false;
-}
-
-void check_game_over(Game *game)
-{
-	Snake *snake = &game->scene.snake;
-	if (ouroboros(snake) || out_of_bounds(snake)) {
-		game->gameover = true;
-	}
-}
-
-void update_game(Game *game)
-{
-	if (game->running && !game->gameover) {
-		// TODO: if the game is running update, else present a pause screen
-		update_scene(game);
-		check_game_over(game);
-	}
-	// TODO: if the player lost the game present a game over screen
-}
 
 void render_snake(Game *game)
 {
@@ -302,13 +185,14 @@ void render_snake(Game *game)
 	DrawRectangleRec(rect, head_color);
 }
 
-void render_apple(Apple apple)
+void render_apple(Game *game)
 {
-	Color apple_color = GetColor(0xFF1818FF);
+	Apple apple = game->scene.apple;
+	Color apple_color = GetColor(APPLE_COLOR);
 	DrawCircle((apple.x + 0.5) * UNIT, (apple.y + 0.5) * UNIT, APPLE_SIZE / 2, apple_color);
 }
 
-void render_ui(Game *game)
+void render_score(Game *game)
 {
 #ifdef CNAKE_DEBUG
 	DrawFPS(10, 10 + UNIT + 10);
@@ -318,13 +202,190 @@ void render_ui(Game *game)
 	DrawText(buf, 10, 10, UNIT*1.5, RED);
 }
 
+void render_pause_screen(Game *game)
+{
+	UNUSED(game);
+
+	Color darker_bg = ColorAlpha(BLACK, 0.3f);
+	DrawRectangle(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, darker_bg);
+
+	int pause_size = UNIT*2;
+	const char *pause = "Game Paused";
+	int pause_width = MeasureText(pause, pause_size);
+	DrawText(pause, (WINDOW_WIDTH - pause_width)/2, WINDOW_HEIGHT/3, pause_size, WHITE);
+
+	int msg_size = UNIT*1.5;
+	const char *msg = "Press `Esc` to unpause";
+	int msg_width = MeasureText(msg, msg_size);
+	DrawText(msg, (WINDOW_WIDTH - msg_width)/2, WINDOW_HEIGHT/3*2, msg_size, WHITE);
+}
+
+void render_gameover_screen(Game *game)
+{
+	UNUSED(game);
+
+	Color darker_bg = ColorAlpha(BLACK, 0.15f);
+	DrawRectangle(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, darker_bg);
+
+	int gameover_size = UNIT*2;
+	const char *gameover = "GAME OVER";
+	int gameover_width = MeasureText(gameover, gameover_size);
+	DrawText(gameover, (WINDOW_WIDTH - gameover_width)/2, WINDOW_HEIGHT/3, gameover_size, RED);
+
+	int msg_size = UNIT*1.5;
+	const char *msg = "Press `Space` to restart";
+	int msg_width = MeasureText(msg, msg_size);
+	DrawText(msg, (WINDOW_WIDTH - msg_width)/2, WINDOW_HEIGHT/3*2, msg_size, RED);
+}
+
+typedef void (*RenderFunc)(Game *);
+
+#define RENDER_COLUMNS 5
+static const RenderFunc renderer[][RENDER_COLUMNS] = {
+	[PLAYING] = {&render_snake, &render_apple, &render_score, NULL},
+	[PAUSED]  = {&render_snake, &render_apple, &render_score, &render_pause_screen, NULL},
+	[GAMEOVER]  = {&render_snake, &render_apple, &render_score, &render_gameover_screen, NULL},
+};
+
+static const char *const WINDOW_TITLE = "Cnake Game";
+
+void init_snake(Snake *snake)
+{
+	snake->dx = 1;
+	snake->dy = 0;
+	snake->dir_changed = false;
+	snake->count = 0;
+
+	// We update the game once before rendering, so we start at -1
+	for (size_t i = 0; i < SNAKE_INIT_LENGTH; i++) {
+		da_append(snake, ((SnakeBody) { i-1, 0, i+1 }));
+	}
+	snake->tail = 0;
+	snake->head = snake->count - 1;
+}
+
+Snake new_snake()
+{
+	Snake snake = {0};
+	init_snake(&snake);
+	return snake;
+}
+
+bool intersects_snake(Snake *snake, Vec2 rect)
+{
+	for (size_t i = 0; i < snake->count; i++) {
+		SnakeBody cur = snake->items[i];
+		if (cur.x == rect.x && cur.y == rect.y) return true;
+	}
+	return false;
+}
+
+Apple new_apple(Scene *scene)
+{
+	Apple apple;
+	do {
+		apple.x = (rand() / (double) RAND_MAX) * (GRID_WIDTH - 1);
+		apple.y = (rand() / (double) RAND_MAX) * (GRID_HEIGHT - 1);
+	} while (intersects_snake(&scene->snake, apple));
+	return apple;
+}
+
+void init_scene(Game *game)
+{
+	game->scene.snake = new_snake();
+	game->scene.apple = new_apple(&game->scene);
+}
+
+bool init_game(Game *game)
+{
+	srand(time(NULL));
+
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+	SetTargetFPS(FPS);
+	SetExitKey(KEY_NULL);
+
+	init_scene(game);
+
+	game->score = 0;
+	// TODO: menu
+	game->state = PLAYING;
+	game->quit = false;
+
+	return true;
+}
+
+void update_scene(Game *game)
+{
+	Scene *scene = &game->scene;
+	Snake *snake = &scene->snake;
+	size_t tail = snake->tail;
+	size_t head = snake->head;
+	SnakeBody old_tail = snake->items[tail];
+	SnakeBody old_head = snake->items[head];
+
+	snake->tail = snake->items[tail].next;
+	snake->items[head].next = tail;
+	snake->head = tail;
+	head = tail;
+
+	snake->items[head].x = old_head.x + snake->dx;
+	snake->items[head].y = old_head.y + snake->dy;
+
+	if (snake->items[head].x == scene->apple.x && snake->items[head].y == scene->apple.y) {
+		da_append(snake, ((SnakeBody) { old_tail.x, old_tail.y, snake->tail }));
+		snake->tail = snake->count - 1;
+		scene->apple = new_apple(scene);
+		game->score += 1;
+	}
+	snake->dir_changed = false;
+}
+
+bool out_of_bounds(Snake *snake)
+{
+	SnakeBody head = snake->items[snake->head];
+	return head.x >= GRID_WIDTH || head.x < 0 || head.y >= GRID_HEIGHT || head.y < 0;
+}
+
+bool ouroboros(Snake *snake)
+{
+	SnakeBody head = snake->items[snake->head];
+	size_t cur = snake->tail;
+	while (cur != snake->head) {
+		SnakeBody b = snake->items[cur];
+		if (b.x == head.x && b.y == head.y) return true;
+		cur = b.next;
+	}
+	return false;
+}
+
+void check_game_over(Game *game)
+{
+	Snake *snake = &game->scene.snake;
+	if (out_of_bounds(snake) || ouroboros(snake)) {
+		game->state = GAMEOVER;
+	}
+}
+
+void update_game(Game *game)
+{
+	if (game->state == PLAYING) {
+		// TODO: if the game is running update, else present a pause screen
+		update_scene(game);
+		check_game_over(game);
+	}
+	// TODO: if the player lost the game present a game over screen
+}
+
 void render(Game *game)
 {
 	BeginDrawing();
-		ClearBackground(GetColor(0x181818FF));
-		render_snake(game);
-		render_apple(game->scene.apple);
-		render_ui(game);
+		ClearBackground(GetColor(BACKGROUND_COLOR));
+		const RenderFunc *pipeline = renderer[game->state];
+		for (int i = 0; i < RENDER_COLUMNS; i++) {
+			RenderFunc f = pipeline[i];
+			if (f == NULL) break;
+			f(game);
+		}
 	EndDrawing();
 }
 
@@ -348,8 +409,7 @@ void reset_game_state(Game *game)
 	init_snake(&game->scene.snake);
 	game->scene.apple = new_apple(&game->scene);
 	game->score = 0;
-	game->gameover = false;
-	game->running = true;
+	game->state = PLAYING;
 }
 
 void handle_keyboard_events(Game *game)
@@ -360,7 +420,7 @@ void handle_keyboard_events(Game *game)
 		switch (key) {
 		case KEY_UP:
 		case KEY_W:
-			if (game->running && !snake->dir_changed && snake->dy == 0) {
+			if (game->state == PLAYING && !snake->dir_changed && snake->dy == 0) {
 				snake->dy = -1;
 				snake->dx = 0;
 				snake->dir_changed = true;
@@ -368,7 +428,7 @@ void handle_keyboard_events(Game *game)
 			break;
 		case KEY_DOWN:
 		case KEY_S:
-			if (game->running && !snake->dir_changed && snake->dy == 0) {
+			if (game->state == PLAYING && !snake->dir_changed && snake->dy == 0) {
 				snake->dy = 1;
 				snake->dx = 0;
 				snake->dir_changed = true;
@@ -376,7 +436,7 @@ void handle_keyboard_events(Game *game)
 			break;
 		case KEY_LEFT:
 		case KEY_A:
-			if (game->running && !snake->dir_changed && snake->dx == 0) {
+			if (game->state == PLAYING && !snake->dir_changed && snake->dx == 0) {
 				snake->dx = -1;
 				snake->dy = 0;
 				snake->dir_changed = true;
@@ -384,19 +444,23 @@ void handle_keyboard_events(Game *game)
 			break;
 		case KEY_RIGHT:
 		case KEY_D:
-			if (game->running && !snake->dir_changed && snake->dx == 0) {
+			if (game->state == PLAYING && !snake->dir_changed && snake->dx == 0) {
 				snake->dx = 1;
 				snake->dy = 0;
 				snake->dir_changed = true;
 			}
 			break;
 		case KEY_SPACE:
-			if (game->gameover) {
+			if (game->state == GAMEOVER) {
 				reset_game_state(game);
 			}
 			break;
 		case KEY_ESCAPE:
-			game->running = !game->running;
+			if (game->state == PAUSED) {
+				game->state = PLAYING;
+			} else if (game->state == PLAYING) {
+				game->state = PAUSED;
+			}
 		default: {}
 		}
 	}
@@ -434,7 +498,6 @@ int main(void)
 	} while (!game.quit);
 
 	CloseWindow();
- 
 defer:
 	free_game(&game);
 	return defer_ret_value;
